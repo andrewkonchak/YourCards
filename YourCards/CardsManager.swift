439ff4a0de4867d03079107bedf21d7e742f3b88
Andrew <andrewkonchak@gmail.com>
2017-11-01 11:56:53 +0200

    1 commit

---
+++ YourCards/CardsManager.swift
@@ -0,0 +1,14 @@
+//
+//  CardsManager.swift
+//  YourCards
+//
+//  Created by Andrew on 10/31/17.
+//  Copyright © 2017 Andrew Konchak. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CardsManager {
+
+}
